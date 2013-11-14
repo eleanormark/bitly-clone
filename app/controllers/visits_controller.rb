4b5ddca654c5eca191caa097eadec57887302bfb
eleanormark <eleanormark@gmail.com>
2013-11-13 17:25:08 -0800

    remove commented-out code

--- app/controllers/visits_controller.rb
+++ app/controllers/visits_controller.rb
@@ -1,7 +1,6 @@
 class VisitsController < ApplicationController
   def show
     @url = ShortenedUrl.find_by(short: params[:slug])
-    # Visit.create(:shortened_url_id => @url.id)
     @url.visits.create
     redirect_to "http://#{@url.original}"
   end
